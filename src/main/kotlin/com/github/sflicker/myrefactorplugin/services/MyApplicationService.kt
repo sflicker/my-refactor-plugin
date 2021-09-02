112ac2f40fbe57b6462fdc4a695b8f0370f7612c
GitHub Action <action@github.com>
2021-09-02 15:24:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/sflicker/myrefactorplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sflicker/myrefactorplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sflicker.myrefactorplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sflicker.myrefactorplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/sflicker/myrefactorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sflicker/myrefactorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sflicker.myrefactorplugin.services
 
+import com.github.sflicker.myrefactorplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
